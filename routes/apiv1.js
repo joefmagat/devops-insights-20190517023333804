3354fe311520c16c79e59b8222dfc9d564a08666
Joef Magat <joefmagat@gmail.com>
2019-05-17 05:40:54 +0000

    country zip test

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -7,8 +7,8 @@ var request = REQUEST.defaults( {
     strictSSL: false
 });
 
-var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
-//var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+//var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
+var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?units=metric";
 
 exports.getWeather = function(req, res) {
 	var zip = req.query.zip;
@@ -47,7 +47,7 @@ exports.getWeather2 = function(req, res) {
 		return res.status(400).send('zip missing');
 	}
 
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	var aurl = OPENWEATHERURL + '&zip=' + zip + ',nz';
 
 	request({
 		method: 'GET',
